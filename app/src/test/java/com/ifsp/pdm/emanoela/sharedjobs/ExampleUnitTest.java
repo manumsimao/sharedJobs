97fbe49b18750e6063c3fa5954e7c9d20a4cde25
Emanoela Masqueti Simão <50922306+manumsimao@users.noreply.github.com>
2021-04-08 21:19:28 -0300

    added the remaining fields

--- app/src/test/java/com/ifsp/pdm/emanoela/sharedjobs/ExampleUnitTest.java
+++ app/src/test/java/com/ifsp/pdm/emanoela/sharedjobs/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.ifsp.pdm.emanoela.sharedjobs;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
